f6a23e3f288ec0671426e77be897e8ddb42b3e06
BossLogic314 <bhagavatulaanish@gmail.com>
2024-05-05 03:36:05 +0530

    Added sanity check for username ans password signup

--- backend-auth/controller/authentication.js
+++ backend-auth/controller/authentication.js
@@ -33,6 +33,30 @@ export let loginUser = async (req, res) => {
     }
 }
 
+const MIN_USERNAME_LENGTH = 7
+const MIN_PASSWORD_LENGTH = 7
+
+function checkCredentials(username, password) {
+
+    if (username.length == 0) {
+        return [false, 'Username cannot be empty. Please choose a valid username'];
+    }
+
+    if (username.length < 7) {
+        return [false, `Username needs to be atleast ${MIN_USERNAME_LENGTH} long`];
+    }
+
+    if (password.length == 0) {
+        return [false, 'Password cannot be empty. Please choose a valid password'];
+    }
+
+    if (password.length < 7) {
+        return [false, `Password needs to be atleast ${MIN_PASSWORD_LENGTH} long`];
+    }
+
+    return [true, 'All good'];
+}
+
 export let signupUser = async (req, res) => {
 
     try {
@@ -40,9 +64,15 @@ export let signupUser = async (req, res) => {
 
         const user = await getUser(username);
         if (user != null) {
-            res.status(405).json({message: `A user already exists with the username '${username}'`});
+            res.status(405).json({message: `A user already exists with the username '${username}. Please choose another username'`});
         }
         else {
+            const [status, message] = checkCredentials(username, password);
+            if (!status) {
+                res.status(401).json({message: message});
+                return;
+            }
+
             const newUser = new userModel({"username": username, "password": password});
             newUser.save();
             res.status(200).json({message: "Successfully signed up!"});
