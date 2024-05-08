93b9d14e51dc36c5df48e6b03055e09ef1920bd8
BossLogic314 <bhagavatulaanish@gmail.com>
2024-05-08 18:17:58 +0530

    Persisting 'createGroupChat' boolean in the local storage

--- frontend/zustand/useCreateGroupChatStore.js
+++ frontend/zustand/useCreateGroupChatStore.js
@@ -1,10 +1,15 @@
 import { create } from "zustand";
+import {devtools, persist} from "zustand/middleware";
 
 const createGroupChatStore = (set) => ({
     createGroupChat: false,
-    setCreateGroupChat: (updatedValue) => {
-        set({createGroupChat: updatedValue});
-    }
+    setCreateGroupChat: (updatedValue) => set({createGroupChat: updatedValue})
 });
 
-export const useCreateGroupChatStore = create(createGroupChatStore);
+export const useCreateGroupChatStore = create(
+    devtools(
+        persist(createGroupChatStore, {
+            name: "createGroupChat"
+        })
+    )
+);
