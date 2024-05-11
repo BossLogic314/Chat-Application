4e15f088aa3d0666f60f04a5f3bc0c0b82f67722
BossLogic314 <bhagavatulaanish@gmail.com>
2024-05-11 05:51:26 +0530

    Made minor changes to the UI

--- frontend/src/app/signup/page.js
+++ frontend/src/app/signup/page.js
@@ -47,7 +47,7 @@ export default function Page() {
 
   return (
     <div className="h-screen w-screen bg-blue-100 flex justify-center items-center">
-      <div className="bg-white rounded-lg px-12 py-8">
+      <div className="bg-white rounded-lg min-h-[350px] px-12 py-8">
         <div className="text-5xl w-80 font-medium text-center">Welcome!</div>
         <div className="mt-3 w-80 text-2xl font-normal">Username</div>
         <input className="bg-gray-100 w-80 text-lg block mt-1 px-3 py-1 border-black border rounded"
@@ -62,7 +62,10 @@ export default function Page() {
         </input>
 
         <div className="loginDiv flex flex-row justify-center">
-          <button className="text-white bg-green-500 hover:bg-green-600 font-medium rounded-lg text-md px-5 py-2.5 mt-2" onClick={signupEntered}>Signup</button>
+          <button className="text-white bg-green-500 hover:bg-green-600 font-medium rounded-lg text-md px-5 py-2.5 mt-2 hover:scale-[1.04] active:scale-[1]"
+          onClick={signupEntered}>
+            Signup
+          </button>
         </div>
 
         <div className="signupMessageDiv w-80 italic mt-2">
